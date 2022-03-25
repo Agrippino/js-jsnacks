1ef8401af308c8bb29b921c65e775ebea71043c3
Agrippino <agrippinorussiello@outlook.it>
2022-03-25 16:31:52 +0100

    aggiunta paresint per far comparire i numeri

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-let primoNumeroInserito = prompt ("Salve, inserisci il primo numero");
+parseInt =let primoNumeroInserito = prompt ("Salve, inserisci il primo numero");
 console.log ("stampo numero 1");
-let secondoNumeroInserito = prompt ("Salve, inserisci il secondo numero");
+parseInt = let secondoNumeroInserito = prompt ("Salve, inserisci il secondo numero");
 console.log ("stampo numero 2");
-let vincitoreGioco;
+let  vincitoreGioco;
 
 if(primoNumeroInserito>secondoNumeroInserito){
-     vicitoreGioco = ("il primo numero è più grande");
-     console.log ("primo numero più grande ");
+    vincitoreGioco = ("il primo numero è più grande");
+    console.log ("primo numero più grande ");
 }
 else if(primoNumeroInserito<secondoNumeroInserito){
-     vicitoreGioco = ("il) secondo numero è più grande");
+    vicitoreGioco = ("il secondo numero è più grande");
     console.log ("secondo numero più grande ");
 }
 else {
-    (vicitoreGioco = "Avete scelto lo stesso numero");
+    vicitoreGioco =("Avete scelto lo stesso numero");
     console.log ("numero uguale");
 }
 document.getElementById("vincitore").innerHTML = vincitoreGioco;
