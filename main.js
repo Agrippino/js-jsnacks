21465c3b3b71e8261d1ad0b6128c41c4d8935d69
Agrippino <agrippinorussiello@outlook.it>
2022-03-25 15:40:35 +0100

    Esercizio 1 finito

---
+++ main.js
@@ -0,0 +1,21 @@
+let primoNumeroInserito = prompt ("Salve, inserisci il primo numero");
+console.log ("stampo numero 1");
+let secondoNumeroInserito = prompt ("Salve, inserisci il secondo numero");
+console.log ("stampo numero 2");
+let vincitoreGioco
+
+if(primoNumeroInserito>secondoNumeroInserito){
+     (vicitoreGioco = "il primo numero è più grande")
+     console.log ("primo numero più grande ");
+}
+else if(primoNumeroInserito<secondoNumeroInserito){
+    ( vicitoreGioco = "il secondo numero è più grande")
+    console.log ("secondo numero più grande ");
+}
+else {
+    (vicitoreGioco = "Avete scelto lo stesso numero");
+    console.log ("numero uguale");
+}
+document.getElementById("vincitore").innerHTML = vincitoreGioco;
+document.getElementById("Primo").innerHTML = primoNumeroInserito;
+document.getElementById("Secondo").innerHTML = secondoNumeroInserito;
