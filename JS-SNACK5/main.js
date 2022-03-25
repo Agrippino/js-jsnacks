1fe4b5d24e0dab1d802caec3d3948093977554a5
Agrippino <agrippinorussiello@outlook.it>
2022-03-25 19:08:56 +0100

    esercizio 5 finito

---
+++ JS-SNACK5/main.js
@@ -0,0 +1,10 @@
+let numeriDispariInseriti = [];
+
+for(let i=0; i<6;i++)
+{ let numeriInseriti= parseInt(prompt("inserisci qui i numeri"));
+    if(numeriDispari = numeriInseriti %2 !=0){
+    numeriDispariInseriti.push (numeriInseriti);
+    console.log(numeriDispariInseriti[i]);
+    document.getElementById("numeri").innerHTML = numeriInseriti;
+}
+}
