f1618842a4190055e59396143caa64c623339479
Agrippino <agrippinorussiello@outlook.it>
2022-03-25 18:14:51 +0100

    Esercizio 4 finito

---
+++ JS-SNACK3/JS-SNACK4/main.js
@@ -0,0 +1,20 @@
+let inserisciTesto = prompt("Il tuo nome è nella lsita?");
+let nomiNellaLista = ["matteo","fabio","pietro","anna","simone","bryan",]
+let nomeRisultato
+let variabileZero = 0
+//ciclo for per confronto con il server
+for (let i=0; i < nomiNellaLista.length;i++){
+    if(nomiNellaLista[i]!=inserisciTesto){     
+    }
+    else {
+        variabileZero ++
+    alert(emailRisultato="Lei è nella lista, entri pure!");
+    break;
+    }
+}
+if (variabileZero == 0){
+    alert(nomeRisultato= "Lei non è presente,vada via");
+}
+
+//reminder fisso in pagina
+document.getElementById("demo").innerHTML = emailRisultato;
