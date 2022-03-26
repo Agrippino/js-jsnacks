95ffa92ab987b7940c1784d0ed9942e095a3c343
Agrippino <agrippinorussiello@outlook.it>
2022-03-26 15:43:46 +0100

    Settimo esercizio finito

---
+++ JS-SNACK1/JS-SNACK7/main.js
@@ -0,0 +1,18 @@
+let primoNumeroInserito = parseInt(prompt("Inserisci il primo numero,grazie"));
+let secondoNumeroInserito= parseInt(prompt("Inserisci il secondo numero,grazie"));
+let risultatoNumero1= primoCubo (primoNumeroInserito);
+let risultatoNumero2= secondoCubo (secondoNumeroInserito);
+
+function primoCubo(numero1) {
+    let primoFattore= (numero1**3);
+    console.log("primo fattore elevato al cubo è uguale a:" + primoFattore);
+    return primoFattore;
+}
+function secondoCubo(numero2) {
+    let secondoFattore = (numero2**3);
+    console.log("secondo fattore elevato al cubo è uguale a:" + secondoFattore);
+    return secondoFattore;
+}
+
+document.getElementById("primo").innerHTML = risultatoNumero1;
+document.getElementById("due").innerHTML = risultatoNumero2;
