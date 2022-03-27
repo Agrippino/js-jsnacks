457cc500954fd828c1bcc3608d2828ccface867b
Agrippino <agrippinorussiello@outlook.it>
2022-03-27 19:11:07 +0200

    aggiunte note

--- JS-SNACK1/JS-SNACK2/main.js
+++ JS-SNACK1/JS-SNACK2/main.js
@@ -1,19 +1,21 @@
+//inserisco le parole
 let inseriscoPrimaParola = prompt("inserisco la prima parola");
 let inseriscoSecondaParola = prompt("inserisco la seconda parola");
 let differenzaParole
+//inserisco una variabile che conta la lunghezza della parola 
 let parola1= inseriscoPrimaParola.length;
 let parola2 = inseriscoSecondaParola.length;
-
+//creo un ciclo che ler confronta 
 if(parola1>parola2){
     (differenzaParole = inseriscoPrimaParola);
-    console.log ("stampo parola1");
+    console.log ("stampo parola1: ");
 }
 else if(parola1<parola2){
     (differenzaParole = inseriscoSecondaParola);
-    console.log ("stampo parola 2");
+    console.log ("stampo parola 2: ");
 }
 else{
-    differenzaParole = ("avete insetiro la stessa parola");
+    differenzaParole = ("la lunghezza della parole è uguale");
 }
 document.getElementById("1parola").innerHTML = inseriscoPrimaParola;
 document.getElementById("2parola").innerHTML = inseriscoSecondaParola;
