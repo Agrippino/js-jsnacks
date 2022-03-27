f2d2b9581230a980d25328e2c734dc178ea1d094
Agrippino <agrippinorussiello@outlook.it>
2022-03-27 19:13:29 +0200

    aggiunte note

--- JS-SNACK1/main.js
+++ JS-SNACK1/main.js
@@ -1,22 +1,25 @@
+//inserisco il primo numero 
 let primoNumeroInserito = prompt ("Salve, inserisci il primo numero");
 parseInt = primoNumeroInserito;
 console.log ("stampo numero 1");
+//inserisco il secondo numero 
 let secondoNumeroInserito = prompt ("Salve, inserisci il secondo numero");
 parseInt = secondoNumeroInserito;
 console.log ("stampo numero 2");
 let  vincitoreGioco;
-
+//creo delle condiozioni 
 if(primoNumeroInserito>secondoNumeroInserito){
     vincitoreGioco = ("il primo numero è più grande");
     console.log ("primo numero più grande ");
 }
 else if(primoNumeroInserito<secondoNumeroInserito){
-    vicitoreGioco = ("il secondo numero è più grande");
+    vincitoreGioco = ("il secondo numero è più grande");
     console.log ("secondo numero più grande ");
 }
 else {
-    vicitoreGioco =("Avete scelto lo stesso numero");
+    vincitoreGioco =("Avete scelto lo stesso numero");
     console.log ("numero uguale");
+    
 }
 document.getElementById("vincitore").innerHTML = vincitoreGioco;
 document.getElementById("Primo").innerHTML = primoNumeroInserito;
