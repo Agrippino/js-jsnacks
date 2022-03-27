fd7e954207cdafd55cbe79e349f23fe52d548007
Agrippino <agrippinorussiello@outlook.it>
2022-03-27 18:48:59 +0200

    inserite anche istruzioni

--- JS-SNACK1/JS-SNACK9/main.js
+++ JS-SNACK1/JS-SNACK9/main.js
@@ -1,15 +1,18 @@
-
+//creo un array vuoto dove poi verranno inseriti i numeri
 let numeriInseritiArray = []; 
 console.log(numeriInseritiArray); 
 sommaNumeri();
- 
+// creo una funziona per eseguire le operazioni
 function sommaNumeri (){ 
 
     let somma = 0; 
     for (let i = 0; i <10; i++) {
+        //inserisco il un push per insrire i numeri in un array 
         numeriInseritiArray.push(parseInt(prompt("insersci qui i numeri")));
+        //eseguo la somma di tutri i numeri presenti 
         somma += numeriInseritiArray[i]; 
         document.getElementById("somma").innerHTML = somma;
+        //faccio la media aritmetica 
         media = somma / 10;
         document.getElementById("media").innerHTML = media;
     }  console.log("risultato media aritmetica: " + media);
