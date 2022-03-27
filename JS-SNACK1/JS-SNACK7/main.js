f4a03c71e9eb195aba4751ee22c639e1bbc99f83
Agrippino <agrippinorussiello@outlook.it>
2022-03-27 18:55:17 +0200

    aggiunte note

--- JS-SNACK1/JS-SNACK7/main.js
+++ JS-SNACK1/JS-SNACK7/main.js
@@ -1,8 +1,9 @@
+//creo due variabili dove verranno inseriti i numeri
 let primoNumeroInserito = parseInt(prompt("Inserisci il primo numero,grazie"));
 let secondoNumeroInserito= parseInt(prompt("Inserisci il secondo numero,grazie"));
 let risultatoNumero1= primoCubo (primoNumeroInserito);
 let risultatoNumero2= secondoCubo (secondoNumeroInserito);
-
+// creo due funzioni che calcolano il cubo dei numeri e lo riportano a video
 function primoCubo(numero1) {
     let primoFattore= (numero1**3);
     console.log("primo fattore elevato al cubo è uguale a:" + primoFattore);
