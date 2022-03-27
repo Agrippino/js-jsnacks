c48362904057db8156066222104fdadb0def5cec
Agrippino <agrippinorussiello@outlook.it>
2022-03-27 19:01:27 +0200

    Aggiunta note

--- JS-SNACK1/JS-SNACK5/main.js
+++ JS-SNACK1/JS-SNACK5/main.js
@@ -1,5 +1,7 @@
+//creo un array dove verrano inseriti i numeri dispari 
 let numeriDispariInseriti = [];
-
+console.log("numeri dispari inseriti: " + numeriDispariInseriti)
+//creo un ciclo dove vengono inseriti 6 numeri e presi esportati solo i dispari
 for(let i=0; i<6;i++)
 { let numeriInseriti= parseInt(prompt("inserisci qui i numeri"));
     if(numeriDispari = numeriInseriti %2 !=0){
