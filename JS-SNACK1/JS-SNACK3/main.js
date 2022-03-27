7b9a2182de79604131c39809315d63155c187589
Agrippino <agrippinorussiello@outlook.it>
2022-03-27 19:06:18 +0200

    aggiunte note

--- JS-SNACK1/JS-SNACK3/main.js
+++ JS-SNACK1/JS-SNACK3/main.js
@@ -1,8 +1,9 @@
+//creo una variabile e un ciclo
 let addizioni = 0
 for (let i=0; i<10; i++)
-{
+{//nel ciclo vengono inseriti i numeri e poi somamti tra loro 
     let inserisciNumeri= parseInt(prompt("insersci qui i numeri"));
     addizioni= addizioni + inserisciNumeri;
-    console.log(addizioni);
+    console.log("numeri per l'addizone: " + addizioni);
     document.getElementById("somma").innerHTML = addizioni;
 }
